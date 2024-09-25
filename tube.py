2541f7178a68e499d839c77f7fd3e0d56e451163
Ximer - Marco <20438890+Ximer612@users.noreply.github.com>
2024-09-25 18:36:32 +0200

    Fixed tube int to float cast

--- tube.py
+++ tube.py
@@ -11,27 +11,25 @@ class tube(game_object):
         self.resetDown = resetDown
         self.resetUp = resetUp
 
-        random_y = 16
+        if not isSecond :
+            random_y = randint(int(pyxel.height-height *self.resetDown),int(pyxel.height+height *self.resetUp))
+        else:
+            random_y = randint(-height // 2,height // 2)
 
         super().__init__(x,random_y,width,height,CollisionType.TUBE,GfxManager.texturesId["tube"])
-
-        if not isSecond :
-            super().__init__(x,randint(pyxel.height-height *self.resetDown,pyxel.height+height *self.resetUp),width,height,CollisionType.TUBE,GfxManager.texturesId["tube"])
-        else:
-            super().__init__(x,randint(-height *0.5,height *0.5),width,height,CollisionType.TUBE,GfxManager.texturesId["tube"])
 
     def update(self):
         if self.x < 0 - self.width*0.5 :
             self.x = pyxel.width + self.width*0.5
             self.setRandomY()
-        self.x -=5
+        self.x -= 5
 
     def setRandomY(self):
 
         if not self.isSecond :
-            self.y = randint(pyxel.height-self.height *self.resetDown,pyxel.height+self.height *self.resetUp)
+            self.y = randint(int(pyxel.height-self.height *self.resetDown),int(pyxel.height+self.height *self.resetUp))
         else:
-            self.y = randint(-self.height *self.resetDown,self.height *self.resetUp)
+            self.y = randint(int(-self.height *self.resetDown),int(self.height *self.resetUp))
 
     def draw(self):
         if not self.isSecond :
